e594637a03c9a9e45a56d0110511746101e58a36
wxqmerge <wxqmerge@outlook.com>
2026-02-23 11:39:18 -0800

    ccs

--- bughouse-ladder-web/src/main.tsx
+++ bughouse-ladder-web/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import '../css/index.css'
+import './css/index.css'
 import App from './App'
 
 createRoot(document.getElementById('root')!).render(
